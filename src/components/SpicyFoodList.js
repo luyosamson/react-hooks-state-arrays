302222de23b8d66303194f3a2f017648ebb1a0f1
luyosamson <luyosamson@gmail.com>
2022-09-27 21:22:20 +0300

    Assignment completed

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -3,14 +3,53 @@ import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy,setFilterBy]=useState("All");
 
   function handleAddFood() {
     const newFood = getNewRandomSpicyFood();
+    const newFoodArray=[...foods,newFood]
+    setFoods(newFoodArray)
     console.log(newFood);
   }
+  function handleLiClick(id){
+    // const newFoodArray=foods.filter((food)=>food.id !==id);
+    // setFoods(newFoodArray)
+    const newFoodArray=foods.map((food)=>{
+      if(food.id===id){
+        return{
+          ...food,
+          heatLevel : food.heatLevel +1,
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
+        };
+      }else{
+
+        return food;
+      }
+
+
+    });
+    setFoods(newFoodArray)
+
+
+  }
+
+
+  function handleFilterChange(event){
+    setFilterBy(event.target.value)
+
+  }
+
+  const foodsToDisplay=foods.filter((food)=>{
+    if(filterBy==="All"){
+      return true;
+    }else{
+      return food.cuisine===filterBy
+    }
+
+  });
+
+  const foodList = foodsToDisplay.map((food) => (
+    <li key={food.id} onClick={()=>handleLiClick(food.id)}>
       {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
     </li>
   ));
@@ -19,7 +58,17 @@ function SpicyFoodList() {
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
       <ul>{foodList}</ul>
+
+    <select name="filter" onChange={handleFilterChange}>
+    <option value="All">All</option>
+    <option value="American">American</option>
+    <option value="Sichuan">Sichuan</option>
+    <option value="Thai">Thai</option>
+    <option value="Mexican">Mexican</option>
+  </select>
     </div>
+
+    
   );
 }
 
